b9e9acf830096887fe135d0ae3d156ecb734f4a1
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-05-31 23:16:25 -0400

    Project Initial Commit

---
+++ TemperatureConversion/TemperatureConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConversionApp.swift
+//  TemperatureConversion
+//
+//  Created by Rodrigo on 31-05-24.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
