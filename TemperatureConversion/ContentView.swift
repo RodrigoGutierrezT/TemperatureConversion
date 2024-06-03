af596162dbd3793e77aef393f6825db1ba57faf7
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-06-03 01:55:52 -0400

    Add temperature convertion functionality

--- TemperatureConversion/ContentView.swift
+++ TemperatureConversion/ContentView.swift
@@ -9,16 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     
-    enum TemperatureType {
-        case ceslsius, kelvin, farenheit
+    enum TemperatureType: String, CaseIterable {
+        case celsius, kelvin, farenheit
     }
     
-    @State private var inputType: TemperatureType = .ceslsius
+    @State private var inputType: TemperatureType = .celsius
     @State private var outputType: TemperatureType = .farenheit
+    
+    @State private var inputNumber: Double = 0
+    private var outputNumber: Double {
+        if inputType == .celsius && outputType == .farenheit{
+            return (inputNumber * 9/5) + 32
+        } else if inputType == .celsius && outputType == .kelvin {
+            return inputNumber + 273.15
+        } else if inputType == .farenheit && outputType == .celsius {
+            return (inputNumber - 32) * 5/9
+        } else if inputType == .farenheit && outputType == .kelvin {
+            let celsius = (inputNumber - 32) * 5/9
+            return celsius + 273.15
+        } else if inputType == .kelvin && outputType == .celsius {
+            return inputNumber - 273.15
+        } else if inputType == .kelvin && outputType == .farenheit {
+            let celsius = inputNumber - 273.15
+            return (celsius * 9/5) + 32
+        } else {
+            return inputNumber
+        }
+    }
     
     var body: some View {
         NavigationStack {
             Form {
+                Section("Input Temperature") {
+                    Picker("Choose input type", selection: $inputType) {
+                        ForEach(TemperatureType.allCases, id: \.self) {
+                            Text($0.rawValue.capitalized)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    TextField("Enter Amount", value: $inputNumber, format: .number)
+                }
+                
+                Section("Output Temperature") {
+                    Picker("Choose output type", selection: $outputType) {
+                        ForEach(TemperatureType.allCases, id: \.self) {
+                            Text($0.rawValue.capitalized)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    Text(outputNumber, format: .number)
+                }
+                
                 
             }
             .navigationTitle("Temperature App")
